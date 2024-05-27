38131cd46dc80c48653acf8a9d5ffb9eebc8d658
WAMBUINGUNGA <wambuiregina28@gmail.com>
2024-05-27 14:21:05 +0300

    new

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count > 0:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared = []
+    for i in int_list:
+        squared.append(i ** 2)
+    return squared
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+# Call the functions to see the output
+happy_new_year()
+
+example_list = [1, 2, 3, 4, 5]
+squared_list = square_integers(example_list)
+print(squared_list)  # Output: [1, 4, 9, 16, 25]
+
+fizzbuzz()
